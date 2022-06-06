0ca3bf68fc173c9a25753e2c0bd7456e4b338e0a
Robert <robertgz@users.noreply.github.com>
2022-06-05 22:05:25 -0700

    Added election-dates.ts

---
+++ src/south-tech/election-dates.ts
@@ -0,0 +1,32 @@
+import * as playwright from 'playwright';
+import { Locator, Page } from 'playwright';
+import { getSearchElectionPage } from './pages';
+
+/**
+ * pathSegment examples: SanDiegoCounty, DavisCity, SantaClaritaCity
+ */
+export const getElectionDates = async (pathSegment: string): Promise<string[]> => {
+  const browser = await playwright.chromium.launch({
+    headless: true,
+  });
+ 
+  const page = await getSearchElectionPage(browser, pathSegment);
+  const yearListLocator = getYearListSelector(page);
+
+  const dates: string[] = [];
+  const count = await yearListLocator.count()
+  for (let i = 1; i < count; ++i) {
+    const content = await yearListLocator.nth(i).locator('td').textContent();
+    if (content) dates.push(content);
+  }
+
+  await browser.close();
+
+  return dates;
+}
+
+const getYearListSelector = (page: Page): Locator => {
+  const yearListTable = `#ctl00_DefaultContent_ASPxRoundPanel1_ASPxDDL_ElectionDate_DDD_L_LBT > tbody  > tr`;
+  
+  return page.locator(yearListTable);
+}
